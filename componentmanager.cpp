f6c75e6428e8fa3af9497a22a2c210afe8367231
andesyv <andesyv@gmail.com>
2019-09-04 15:57:07 +0200

    Completed adding of components
    Atleast I think it works like I hoped it would. :s

--- componentmanager.cpp
+++ componentmanager.cpp
@@ -1,6 +1,17 @@
 #include "componentmanager.h"
 
+template <typename T>
+unsigned int ComponentManager::DataArray<T>::mLength = 0;
+
 ComponentManager::ComponentManager()
 {
 
+}
+
+void ComponentManager::resizeArrays(unsigned int newSize) {
+    mTransforms.resize(newSize);
+    mRenders.resize(newSize);
+
+    mTransforms.mLength = newSize;
+    arrayLength = newSize;
 }
--- componentmanager.h
+++ componentmanager.h
@@ -3,6 +3,7 @@
 
 #include "componentdata.h"
 #include <typeinfo>
+#include <cassert>
 
 /** A component data manager class
  * Constructs entities and manages
@@ -18,14 +19,45 @@ private:
     template <class T>
     class DataArray
     {
-    private:
-        T* data = nullptr;
-        static unsigned int length;
+        // No need to make the member functions private, as they are only
+        // accessible from within ComponentManager
+    public:
+        // Data pointer to beginning of normal c-style array.
+        T* mData = nullptr;
+        static unsigned int mLength;
+
+        // internalLength should always be the same as length
+        // (only used when resizing)
+        unsigned int mInternalLength;
+
+        void resize(unsigned int newLength)
+        {
+            // Copy content to temp array
+            T *temp = new T[mInternalLength];
+            for (unsigned int i{0}; i < mInternalLength; ++i)
+                temp[i] = mData[i];
+
+            // Make new array
+            delete[] mData;
+            mData = new T[newLength];
+
+            // Fill array again
+            for (unsigned int i{0}; i < newLength; ++i) {
+                // Default constructs objects outside of array
+                mData[i] = (i < mInternalLength) ? temp[i] : T{};
+            }
+
+            // Cleanup
+            delete[] temp;
+            mInternalLength = newLength;
+        }
 
     public:
-        T* get() { return data; }
+        T* get() { return mData; }
+        T* operator* () { return mData; }
+        T& operator[] (unsigned int i) {return *(mData + i); }
+        T& at (unsigned int i) { assert(i < mLength); return *(mData + i); }
 
-        // template <class G>
         class DataArrayIterator
         {
         private:
@@ -41,7 +73,7 @@ private:
         };
 
         DataArrayIterator begin() { return DataArrayIterator{*this, 0}; }
-        DataArrayIterator end() { return DataArrayIterator{*this, length}; }
+        DataArrayIterator end() { return DataArrayIterator{*this, mLength}; }
     };
 
 
@@ -51,10 +83,10 @@ private:
     static constexpr unsigned int componentCount{2};
 
     // Component arrays. Remember to update componentCount if adding more.
-    Transform *mTransforms{nullptr};
-    Render *mRenders{nullptr};
-    // DataArray<Transform> mTransforms;
-    // DataArray<Render> mRenders;
+    // Transform *mTransforms{nullptr};
+    // Render *mRenders{nullptr};
+    DataArray<Transform> mTransforms;
+    DataArray<Render> mRenders;
 
     // Length of all component arrays.
     unsigned int arrayLength{0};
@@ -119,52 +151,84 @@ private:
         return HorizontalIteratorWrapper{this, index};
     }
 
-    void resizeArray(unsigned int newSize) {
-        Transform *temp = new Transform[arrayLength];
-        for (unsigned int i{0}; i < arrayLength; ++i)
-            temp[i] = mTransforms[i];
+    void resizeArrays(unsigned int newSize);
 
-        delete[] mTransforms;
-        mTransforms = new Transform[newSize];
-
-        for (unsigned int i{0}; i < arrayLength; ++i)
-            temp[i] = mTransforms[i];
-
-        arrayLength = newSize;
-    }
 public:
     ComponentManager();
 
     unsigned int createEntity();
+
     template <typename T>
     void addComponent(unsigned int entity)
     {
-        long int internalIndex{-1};
+        long int internalIndex{-1}, emptyRow{-1};
         for (unsigned int i{0}; i < arrayLength && internalIndex != -1; ++i)
         {
+            bool rowIsEmpty{true};
             for (auto comp : loopHorizontal(i)) {
-                if (comp->valid && comp->entityId == entity) {
-                    // This component is part of same entity
-                    internalIndex = i;
-                    break;
+                if (comp->valid) {
+                    if (comp->entityId == entity) {
+                        // This component is part of same entity
+                        internalIndex = i;
+                        // break;
+                    }
+
+                    rowIsEmpty = false;
                 }
             }
+            if (rowIsEmpty)
+                emptyRow = i;
         }
 
         // If internalIndex isn't -1, we have found other components
         // that belong to the same entity
         if (-1 < internalIndex)
         {
-
+            if (typeid (T) == typeid(Transform)) {
+                if (mTransforms[internalIndex].valid) {
+                    // Adding component on already excisting component
+                    /* In the scenario that we try to add a component to an
+                     * already excisting component for the same entity, we
+                     * should just return out.
+                     * All components on a row (should) belongs to the same entity,
+                     * so we won't need to check that the component is
+                     * of the same entity aswell.
+                     */
+                    return;
+                } else {
+                    // Object isn't used, you are free to place a component there
+                    mTransforms[internalIndex] = T{};
+                }
+            } else if (typeid (T) == typeid(Render)) {
+                if (mRenders[internalIndex].valid) {
+                    // Adding component on already excisting component
+                    /* In the scenario that we try to add a component to an
+                     * already excisting component for the same entity, we
+                     * should just return out.
+                     * All components on a row (should) belongs to the same entity,
+                     * so we won't need to check that the component is
+                     * of the same entity aswell.
+                     */
+                    return;
+                } else {
+                    // Object isn't used, you are free to place a component there
+                    mRenders[internalIndex] = T{};
+                }
+            }
         }
         // If internalIndex is -1, we didn't find any other components
         // that belongs to entity. A.k.a., this is a new entity.
         else
         {
+            // If no empty rows were found, add a new row and add components to
+            // that row
+            if (emptyRow < 0)
+                resizeArrays(arrayLength + 1);
+
             if (typeid(T) == typeid(Transform)) {
-
+                mTransforms.at((emptyRow < 0) ? arrayLength : emptyRow) = T{};
             } else if (typeid(T) == typeid(Render)) {
-
+                mRenders.at((emptyRow < 0) ? arrayLength : emptyRow) = T{};
             }
         }
     }

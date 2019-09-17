4085e3b5c3d6592f7a9903ad65815fbfed2cec6b
Skau <kskau93@gmail.com>
2019-09-17 17:47:22 +0200

    Bug fix

--- Widgets/soundwidget.h
+++ Widgets/soundwidget.h
@@ -1,11 +1,17 @@
 #ifndef SOUNDWIDGET_H
 #define SOUNDWIDGET_H
 
+#include "componentwidget.h"
 
 class SoundWidget : public ComponentWidget
 {
 public:
-    SoundWidget();
+    SoundWidget(MainWindow *mainWindow, QWidget* parent = nullptr);
+
+    void updateData();
+
+protected slots:
+    void Remove();
 };
 
 #endif // SOUNDWIDGET_H

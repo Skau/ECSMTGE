cf912e287e2ca4766ca5df8beafe37d21721e3e2
Kristoffer Skau <kskau93@gmail.com>
2019-11-24 23:21:32 +0100

    Forgot to update the header file..

--- scriptsystem.h
+++ scriptsystem.h
@@ -233,7 +233,7 @@ private:
     /**
      * @brief Updates all CPP components based on the ones used in JS. Called once at the start of JS scripts.
      */
-    QJsonObject updateCPPComponent(ScriptComponent& comp);
+    void updateCPPComponent(ScriptComponent& comp, std::vector<QJsonObject>& deferredSpawning);
     void updateCPPComponent(ScriptComponent& comp, QJSValue compList);
 
     void initializeJSEntity(ScriptComponent &comp);
